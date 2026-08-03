205bb9a163906077ac975f2c7a62ef2b5ecd993e
vishnumatamala <vishnuvardhanreddy@speshway.com>
2026-08-03 16:49:41 +0530

    feat: complete enterprise microservices migration, auth-service & user-service view templates

---
+++ user-service/src/main/java/com/crm/user/config/MailConfig.java
@@ -0,0 +1,20 @@
+package com.crm.user.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(JavaMailSender.class)
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("localhost");
+        mailSender.setPort(25);
+        return mailSender;
+    }
+}
